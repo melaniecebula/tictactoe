b32c173fd9dca4f97ff154aa03b3dfca6ffce885
Melanie Cebula <melaniecebula@melaniecebu-mbp.local>
2013-07-08 22:01:22 -0700

    "halp"

--- TicTacToe/TicTacToeViewController.h
+++ TicTacToe/TicTacToeViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TicTacToeViewController : UIViewController
-
+@interface TicTacToeViewController : UIViewController {
+    
+}
+ @property (weak, nonatomic) IBOutlet UILabel *userScore;
+ @property (weak, nonatomic) IBOutlet UILabel *npcScore;
+ @property (weak, nonatomic) IBOutlet UIButton *topLeftField;
+ @property (weak, nonatomic) IBOutlet UIButton *topCenterField;
+ @property (weak, nonatomic) IBOutlet UIButton *topRightField;
+ @property (weak, nonatomic) IBOutlet UIButton *midLeftField;
+ @property (weak, nonatomic) IBOutlet UIButton *midCenterField;
+ @property (weak, nonatomic) IBOutlet UIButton *midRightField;
+ @property (weak, nonatomic) IBOutlet UIButton *botLeftField;
+ @property (weak, nonatomic) IBOutlet UIButton *botCenterField;
+ @property (weak, nonatomic) IBOutlet UIButton *botRightField;
+@property UISearchDisplayController *watwat;
 @end
